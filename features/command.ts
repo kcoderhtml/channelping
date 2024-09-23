883d26fbe05d31a067fe8e6b38aa5244ca8098ab
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-09-23 12:26:08 -0400

    feat: allow non admins to ping if they created the channel

--- features/command.ts
+++ features/command.ts
@@ -4,32 +4,36 @@ const command = async () => {
     slackApp.command('/createchannelping', async ({ context, payload }) => {
         // check whether the user is an admin
         if (!process.env.ADMINS?.split(',').includes(context.userId!)) {
-            await context.respond({
-                text: "Sorry but you aren't authorized to use this!",
+            // check if they are a channel manager for this channel
+            const channelInfo = await slackClient.conversations.info({
+                channel: payload.channel_id!,
             })
 
-            return
+            if (channelInfo.channel?.creator !== context.userId!) {
+                await context.respond({
+                    text: "Sorry but you aren't authorized to use this!",
+                })
+
+                return
+            }
         }
 
         const members = await slackClient.conversations
             .members({
                 channel: payload.channel_id!,
             })
-            .then((res) => res.members)
+            .then((res) => res.members?.length)
 
         await slackClient.chat.postEphemeral({
             channel: payload.channel_id!,
             user: context.userId!,
-            text:
-                'Do you want to make a ping group for ' +
-                members?.length +
-                ' members?',
+            text: `Do you want to make a ping group for ${members} members?`,
             blocks: [
                 {
                     type: 'section',
                     text: {
                         type: 'mrkdwn',
-                        text: 'Are you sure you want to make a ping group for 9 members?',
+                        text: `Are you sure you want to make a ping group for ${members} members?`,
                     },
                 },
                 {
