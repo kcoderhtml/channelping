a047c0beda4b28ee353946b411ed22462cce94fd
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-05 16:11:17 -0400

    bug: fix reload apphome

--- features/reloadHandler.ts
+++ features/reloadHandler.ts
@@ -1,50 +1,66 @@
 import { slackApp } from '../index'
-import { blog, clog } from '../lib/Logger'
-import { getSettingsMenuBlocks } from './appHome'
 
 const reloadHandler = async () => {
     // listen for action
     slackApp.action('reloadDashboard', async ({ payload, context }) => {
         // get info about the user
-        const user = await context.client.users.info({
-            user: payload.user.id,
+        await context.client.views.publish({
+            user_id: payload.user.id,
+            view: {
+                type: 'home',
+                blocks: [
+                    {
+                        type: 'section',
+                        text: {
+                            type: 'mrkdwn',
+                            text: `:gear: ${name}'s Dashboard :gear:`,
+                        },
+                    },
+                    {
+                        type: 'divider',
+                    },
+                    {
+                        type: 'section',
+                        text: {
+                            type: 'mrkdwn',
+                            text: `Hi <@${payload.user.id}> to use me run \`/createchannelping\` this will only work if you own the channel. Have fun :)`,
+                        },
+                    },
+                    {
+                        type: 'divider',
+                    },
+                    {
+                        type: 'section',
+                        text: {
+                            type: 'mrkdwn',
+                            text: `:blobby-admission_tickets: Admins: \n\n${process.env.ADMINS?.split(
+                                ','
+                            )
+                                .map((admin) => `<@${admin}>`)
+                                .join(' ')}`,
+                        },
+                    },
+                    {
+                        type: 'divider',
+                    },
+                    {
+                        type: 'actions',
+                        elements: [
+                            {
+                                type: 'button',
+                                text: {
+                                    type: 'plain_text',
+                                    text: 'Reload Dashboard',
+                                    emoji: true,
+                                },
+                                action_id: 'reloadDashboard',
+                            },
+                        ],
+                    },
+                ],
+            },
         })
-
-        // check if the user is authorized
-        if (
-            user.user?.is_owner ||
-            user.user?.is_admin ||
-            process.env.ADMINS?.split(',').includes(user.user?.id!)
-        ) {
-            clog(
-                `User <@${user.user!.id}> is authorized to reload the settings page.`,
-                'info'
-            )
-
-            // update the home tab
-            await context.client.views.publish({
-                user_id: payload.user.id,
-                view: {
-                    type: 'home',
-                    blocks: await getSettingsMenuBlocks(true, payload.user.id),
-                },
-            })
-            return
-        } else {
-            blog(
-                `User <@${user.user!.id}> is not authorized to reload the settings page.`,
-                'error'
-            )
-            // update the home tab
-            await context.client.views.publish({
-                user_id: payload.user.id,
-                view: {
-                    type: 'home',
-                    blocks: await getSettingsMenuBlocks(false, payload.user.id),
-                },
-            })
-            return
-        }
+        return
     })
 }
 
